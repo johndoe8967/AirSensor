79184806e0826129fb6f3ac6da7b6f36a28ce7bf
JOHNDOE8967-PC\johndoe8967 <johndoe8967@gmx.at>
2023-05-05 10:04:49 +0200

    Anpassungen an neue ESP Firmware

--- Remote_Feldkirchen.ino
+++ Remote_Feldkirchen.ino
@@ -14,8 +14,6 @@ DHT anschluß
 4 GND
 10k zwischen 1 und 2
 
-Spannungsmeesung A0
-
 ESP Anbindung
 +5V
 GND
@@ -27,107 +25,90 @@ ESP D8 -> Nano D3
 #include <DHT.h>
 #include <Wire.h>
 #include <BME280I2C.h>
-  #define SERIAL_BAUD 115200
+#define SERIAL_BAUD 115200
 
 //#define debug
 #ifdef debug
-  #define mySerial Serial
-  #define DelayCount 200
+#define mySerial Serial
+#define DelayCount 200
 #else
-  #include <SoftwareSerial.h>
-  SoftwareSerial mySerial(3,2); //(D3=RX,D2=TX Nano) (D8=TX,D7=RX ESP)
-  #define DelayCount 140
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3, 2);  //(D3=RX,D2=TX Nano) (D8=TX,D7=RX ESP)
+#define DelayCount 140
 #endif
 
-const int analogInPin0 = A0;  // Analog input pin Spannung
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-float Spannung = 0.0;
-int ledState = LOW;             // ledState used to set the LED
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+int ledState = LOW;              // ledState used to set the LED
 
 
-BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
-                  // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
+BME280I2C bme;  // Default : forced mode, standby time = 1000 ms
+                // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 float Luftdruck = 0.0;
-float Aussentemp = 0.0;
 
-#define DHTPIN 4     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT22
-DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
-float Feuchte = 0.0;  //Stores humidity value
-float Innentemp = 0.0; //Stores temperature value
+#define DHTPIN 4           // what pin we're connected to
+#define DHTTYPE DHT22      // DHT22
+DHT dht(DHTPIN, DHTTYPE);  // Initialize DHT sensor for normal 16mhz Arduino
+float Feuchte = 0.0;       //Stores humidity value
+float Innentemp = 0.0;     //Stores temperature value
 
-String inputString = "";          // a String to hold incoming data
-bool stringComplete = false;      // whether the string is complete
+float CO_2 = 0.0;  //Stores CO2 concentration
+
+String inputString = "";      // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
 
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(SERIAL_BAUD);
   mySerial.begin(9600);
-  analogReference(INTERNAL);  //1,1Volt Referenz
-  inputString.reserve(50);       // reserve 200 bytes for the inputString:  
+  inputString.reserve(50);  // reserve 200 bytes for the inputString:
   dht.begin();
-   pinMode(ledPin, OUTPUT);
-while(!Serial) {} // Wait
+  pinMode(ledPin, OUTPUT);
+  while (!Serial) {}  // Wait
 
   Wire.begin();
-  
-  while(!bme.begin())
-  {
+
+  while (!bme.begin()) {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
-   switch(bme.chipModel())
-  {
-     case BME280::ChipModel_BME280:
-       Serial.println("Found BME280 sensor! Success.");
-       break;
-     case BME280::ChipModel_BMP280:
-       //Serial.println("Found BMP280 sensor! No Humidity available.");
-       break;
-     default:
-       Serial.println("Found UNKNOWN sensor! Error!");
+  switch (bme.chipModel()) {
+    case BME280::ChipModel_BME280:
+      Serial.println("Found BME280 sensor! Success.");
+      break;
+    case BME280::ChipModel_BMP280:
+      //Serial.println("Found BMP280 sensor! No Humidity available.");
+      break;
+    default:
+      Serial.println("Found UNKNOWN sensor! Error!");
   }
-  
 }
 
 void send2ESP8266() {
   // print the results to the ESP8266:
-  mySerial.print("RT_ist"); 
-  mySerial.print(Feuchte);//rel.Feuchte
-  mySerial.print("\n\r");
-  
-  mySerial.print("AT"); 
-  mySerial.print(Innentemp);//Innentemp
-  mySerial.print("\n\r");
-  
-  mySerial.print("RT_soll"); 
-  mySerial.print(Aussentemp);//Außentemp
+  mySerial.print("HUMI");
+  mySerial.print(Feuchte);  //rel.Feuchte
   mySerial.print("\n\r");
 
-  mySerial.print("KT_soll"); 
-  mySerial.print(Luftdruck);//Luftdruck
+  mySerial.print("TEMP");
+  mySerial.print(Innentemp);  //Innentemp
   mySerial.print("\n\r");
 
-  mySerial.print("KT_ist"); 
-  mySerial.print(Spannung*18.75/1000); //Spannung
+  mySerial.print("PRESS");
+  mySerial.print(Luftdruck);  //Luftdruck
   mySerial.print("\n\r");
 
-  mySerial.print("Brennerstd"); 
-  mySerial.print(Luftdruck*3600); //
+  mySerial.print("CO_2");
+  mySerial.print(CO_2);
   mySerial.print("\n\r");
-
-/*  mySerial.print("FuellStand"); 
-  mySerial.print(0);
-  mySerial.print("\n\r");*/
 }
 
-unsigned char counter=0;
+unsigned char counter = 0;
 void loop() {
   counter++;
   if (counter == DelayCount) {
     counter = 0;
-    readAndCalcBME280Data();    
+    readAndCalcBME280Data();
     //Read data and store it to variables hum and temp
     Feuchte = dht.readHumidity();
     if (isnan(Feuchte)) Serial.println("Error reading Feuchte");
@@ -135,13 +116,12 @@ void loop() {
       Serial.print("Feuchte: ");
       Serial.println(Feuchte);
     }
-    Innentemp= dht.readTemperature()+1.0;
+    Innentemp = dht.readTemperature()+1.0;
     if (isnan(Innentemp)) Serial.println("Error reading Innentemp");
     else {
       Serial.print("Innentemp: ");
       Serial.println(Innentemp);
     }
-    Spannung = analogRead(analogInPin0);
     delay(1910);
     if (ledState == LOW) {
       ledState = HIGH;
@@ -153,7 +133,7 @@ void loop() {
     digitalWrite(ledPin, ledState);
     send2ESP8266();
   }
-  
+
   while (mySerial.available()) {
     // get the new byte:
     char inChar = (char)mySerial.read();
@@ -172,22 +152,23 @@ void loop() {
     stringComplete = false;
   }
 
-  
+
   // wait 200 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(200);
-   
 }
 
-void readAndCalcBME280Data()
-{
-   float temp(NAN), hum (NAN), pres(NAN);
+void readAndCalcBME280Data() {
+  float temp(NAN), hum(NAN), pres(NAN);
 
-   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+  BME280::PresUnit presUnit(BME280::PresUnit_Pa);
 
-   bme.read(pres,temp, hum,  tempUnit, presUnit);
+  bme.read(pres, temp, hum, tempUnit, presUnit);
 
-   Aussentemp=temp-1.4;
-   Luftdruck=(pres/100+31.5);
+  Luftdruck = (pres / 100 + 31.5);
+  Serial.print("Druck: ");
+  Serial.println(Luftdruck);
+  Serial.print("Temp: ");
+  Serial.println(temp);
 }
